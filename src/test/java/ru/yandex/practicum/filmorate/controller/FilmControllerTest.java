e95cdc619aa8056e832558f7eb1775f32f45adb6
Kirill Maltsev <opp.2211@gmail.com>
2023-03-09 23:30:17 +0500

    test: добавить тесты для валидации

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -0,0 +1,102 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FilmControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
+
+    @SneakyThrows
+    @Test
+    void addValidFilm() {
+        Film film = Film.builder()
+                .name("film1")
+                .description("filmDescription1")
+                .duration(100)
+                .releaseDate(LocalDate.of(2014,5,12))
+                .build();
+
+        mockMvc.perform(post("/films")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(film)))
+                .andExpect(status().isOk());
+    }
+    @SneakyThrows
+    @Test
+    void addBlankNameFilm() {
+        Film film = Film.builder()
+                .name(" ")
+                .description("filmDescription1")
+                .duration(100)
+                .releaseDate(LocalDate.of(2014,5,12))
+                .build();
+
+        mockMvc.perform(post("/films")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(film)))
+                .andExpect(status().isBadRequest());
+    }
+    @SneakyThrows
+    @Test
+    void addExceededDescriptionFilm() {
+        Film film = Film.builder()
+                .name("film1")
+                .description("A".repeat(201))
+                .duration(100)
+                .releaseDate(LocalDate.of(2014,5,12))
+                .build();
+
+        mockMvc.perform(post("/films")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(film)))
+                .andExpect(status().isBadRequest());
+    }
+    @SneakyThrows
+    @Test
+    void addInvalidReleaseDateFilm()
+    {
+        Film film = Film.builder()
+                .name("film1")
+                .description("filmDescription1")
+                .duration(100)
+                .releaseDate(LocalDate.of(1895,12,27))
+                .build();
+
+        mockMvc.perform(post("/films")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(film)))
+                .andExpect(status().isBadRequest());
+    }
+    @SneakyThrows
+    @Test
+    void addNegativeDurationFilm() {
+        Film film = Film.builder()
+                .name("film1")
+                .description("filmDescription1")
+                .duration(-100)
+                .releaseDate(LocalDate.of(2014,5,12))
+                .build();
+
+        mockMvc.perform(post("/films")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(film)))
+                .andExpect(status().isBadRequest());
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -0,0 +1,119 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
+
+    @SneakyThrows
+    @Test
+    void addValidUser() {
+        User user = User.builder()
+                .email("example@yandex.ru")
+                .login("login1")
+                .name("name1")
+                .birthday(LocalDate.of(1990,8,11))
+                .build();
+
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(user)))
+                .andExpect(status().isOk());
+    }
+    @SneakyThrows
+    @Test
+    void addUserBlankName() {
+        String login = "login1";
+        User user = User.builder()
+                .email("example@yandex.ru")
+                .login("login1")
+                .name(" ")
+                .birthday(LocalDate.of(1990,8,11))
+                .build();
+
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(login));
+    }
+    @SneakyThrows
+    @Test
+    void addInvalidEmailUser() {
+        User user = User.builder()
+                .email("example#yandex.ru")
+                .login("login1")
+                .name("name1")
+                .birthday(LocalDate.of(1990,8,11))
+                .build();
+
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(user)))
+                .andExpect(status().isBadRequest());
+    }
+    @SneakyThrows
+    @Test
+    void addBlankLoginUser() {
+        User user = User.builder()
+                .email("example@yandex.ru")
+                .login(" ")
+                .name("name1")
+                .birthday(LocalDate.of(1990,8,11))
+                .build();
+
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(user)))
+                .andExpect(status().isBadRequest());
+    }
+    @SneakyThrows
+    @Test
+    void addUserLoginContainsSpaces() {
+        User user = User.builder()
+                .email("example@yandex.ru")
+                .login("log in1")
+                .name("name1")
+                .birthday(LocalDate.of(1990,8,11))
+                .build();
+
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(user)))
+                .andExpect(status().isBadRequest());
+    }
+    @SneakyThrows
+    @Test
+    void addInvalidBirthdayUser() {
+        User user = User.builder()
+                .email("example@yandex.ru")
+                .login("login1")
+                .name("name1")
+                .birthday(LocalDate.of(2024,8,11))
+                .build();
+
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(user)))
+                .andExpect(status().isBadRequest());
+    }
+}
