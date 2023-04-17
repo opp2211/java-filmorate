c1b1a2b487377bcfe4bda697981e03cbfc6271b1
Kirill Maltsev <opp.2211@gmail.com>
2023-04-18 03:12:14 +0500

    feat: создать дао для film_genre

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/FilmGenreDbStorage.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.filmorate.storage.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.storage.interfaces.FilmGenreStorage;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Repository
+@RequiredArgsConstructor
+public class FilmGenreDbStorage implements FilmGenreStorage {
+    JdbcTemplate jdbcTemplate;
+    @Override
+    public void batchAddFilmGenre(int filmId, int[] genreIds) {
+        String sql = "INSERT INTO film_genre (film_id, genre_id) VALUES (?, ?) ";
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setInt(1, filmId);
+                ps.setInt(2, genreIds[i]);
+            }
+
+            @Override
+            public int getBatchSize() {
+                return genreIds.length;
+            }
+        });
+    }
+
+    @Override
+    public void removeFilmGenre(int filmId) {
+        String sql = "DELETE FROM film_genre WHERE film_id = ?";
+        jdbcTemplate.update(sql, filmId);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/FilmGenreStorage.java
@@ -0,0 +1,6 @@
+package ru.yandex.practicum.filmorate.storage.interfaces;
+
+public interface FilmGenreStorage {
+    void batchAddFilmGenre(int filmId, int[] genreIds);
+    void removeFilmGenre(int filmId);
+}
