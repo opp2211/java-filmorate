f9331d15354aaf1a6c5df7d06490b2998cccc5e4
Kirill Maltsev <opp.2211@gmail.com>
2023-04-17 05:29:10 +0500

    feat: заменить INSERT на MERGE

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,16 @@
-INSERT INTO mpa (name) VALUES ('G');
-INSERT INTO mpa (name) VALUES ('PG');
-INSERT INTO mpa (name) VALUES ('PG-13');
-INSERT INTO mpa (name) VALUES ('R');
-INSERT INTO mpa (name) VALUES ('NC-17');
+MERGE INTO mpa (mpa_id, name)
+KEY(mpa_id) VALUES
+(1, 'G'),
+(2, 'PG'),
+(3, 'PG-13'),
+(4, 'R'),
+(5, 'NC-17');
 
-INSERT INTO genre (name) VALUES ('Комедия');
-INSERT INTO genre (name) VALUES ('Драма');
-INSERT INTO genre (name) VALUES ('Мультфильм');
-INSERT INTO genre (name) VALUES ('Триллер');
-INSERT INTO genre (name) VALUES ('Документальный');
-INSERT INTO genre (name) VALUES ('Боевик');
+MERGE INTO genre (genre_id, name)
+KEY(genre_id) VALUES
+(1, 'Комедия'),
+(2, 'Драма'),
+(3, 'Мультфильм'),
+(4, 'Триллер'),
+(5, 'Документальный'),
+(6, 'Боевик');
